e40dc1781886753840edddbfb552da91c19e2bb7
Ryan Carniato <ryansolid@gmail.com>
2019-06-07 19:58:35 -0700

    fix typescript output during dev

--- scripts/start.js
+++ scripts/start.js
@@ -100,7 +100,7 @@ checkBrowsers(paths.appPath, isInteractive)
       devSocket,
       urls,
       useYarn,
-      useTypeScript,
+      useTypeScript: false,
       webpack,
     });
     // Load proxy config

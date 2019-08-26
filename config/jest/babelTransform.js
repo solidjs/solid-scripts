3927f9ea606f156cf9750e411d033fc933cf1d25
Ryan Carniato <ryansolid@gmail.com>
2019-08-25 19:45:18 -0700

    fix jest config

--- config/jest/babelTransform.js
+++ config/jest/babelTransform.js
@@ -1,8 +1,7 @@
 const babelJest = require('babel-jest');
 
 module.exports = babelJest.createTransformer({
-  presets: [require.resolve('@babel/preset-env')],
-  plugins: [require.resolve('babel-plugin-jsx-dom-expressions')],
+  presets: [require.resolve('@babel/preset-env'), require.resolve('babel-preset-solid')],
   babelrc: false,
   configFile: false,
 });

09dcae83d178d98e2ebfb7def2a39be129622a5d
Son <leson.phung@gmail.com>
2020-11-04 21:38:23 +1100

    feat: add preset-typescript into babel so that we can write typescript in test

--- config/jest/babelTransform.js
+++ config/jest/babelTransform.js
@@ -1,7 +1,7 @@
 const babelJest = require('babel-jest');
 
 module.exports = babelJest.createTransformer({
-  presets: [require.resolve('@babel/preset-env'), require.resolve('babel-preset-solid')],
+  presets: [require.resolve('@babel/preset-env'), require.resolve('@babel/preset-typescript'), require.resolve('babel-preset-solid')],
   babelrc: false,
   configFile: false,
 });
